1fe80e20e943e8a7b7dacd4c75a8d8c6079ebe64
rknizzle <ryan@autotiv.com>
2025-02-15 08:59:17 -0500

    STUB

--- setup.py
+++ setup.py
@@ -41,6 +41,11 @@ setup(
                  'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
                  'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9',
+                 'Programming Language :: Python :: 3.10',
+                 'Programming Language :: Python :: 3.11',
+                 'Programming Language :: Python :: 3.12',
                  'Topic :: Multimedia :: Graphics :: 3D Modeling',
                  ],
 )
